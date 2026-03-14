aa10ba9960dbd106fff73371ff53ded0bf21c288
130sf <mdens@gmx.net>
2026-03-14 09:55:11 +0100

    depl_2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,13 @@
-import { Switch, Route } from "wouter";
+﻿import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Dashboard from "./pages/Dashboard";
-import { disableReactDevTools } from “@fvilers/disable-react-devtools”;
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools ()
-
-  disableReactDevTools
+if (import.meta.env.MODE === 'production') disableReactDevTools()
 
 function Router() {
   return (
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
